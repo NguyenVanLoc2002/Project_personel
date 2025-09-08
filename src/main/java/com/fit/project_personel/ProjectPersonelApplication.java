718449791ac5c67c9ab4e88fd772cd5d972a20ab
NguyenVanLoc2002 <nguyenvanloc6756@gmail.com>
2025-09-08 22:04:58 +0700

    first commit

---
+++ src/main/java/com/fit/project_personel/ProjectPersonelApplication.java
@@ -0,0 +1,13 @@
+package com.fit.project_personel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPersonelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPersonelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fit/project_personel/ProjectPersonelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fit.project_personel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPersonelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
